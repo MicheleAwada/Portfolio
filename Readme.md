9ec482c7cfae0ac67e9b9996545058fdb9cbcd29
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2023-10-05 16:00:31 +0300

    Completed Portfolio

---
+++ Readme.md
@@ -0,0 +1,8 @@
+create virtual env
+do "pip install -r requirements.txt"
+
+than go inside portfolio
+than again inside portfolio
+
+add a env file for the info of postgres db, and options like debug, allowedhosts and secret key
+than say "python manage.py runserver"
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 handler404 = 'misc.views.custom404'
